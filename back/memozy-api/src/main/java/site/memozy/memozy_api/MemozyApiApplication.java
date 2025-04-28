c179788dd0d8e51e7315080ebd1e0138ad2685cc
TinyFrogs <qns0147@gmail.com>
2025-04-28 15:06:06 +0900

    Chore: initialize project structure with Gradle and Spring Boot configuration

---
+++ back/memozy-api/src/main/java/site/memozy/memozy_api/MemozyApiApplication.java
@@ -0,0 +1,13 @@
+package site.memozy.memozy_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemozyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemozyApiApplication.class, args);
+	}
+
+}
