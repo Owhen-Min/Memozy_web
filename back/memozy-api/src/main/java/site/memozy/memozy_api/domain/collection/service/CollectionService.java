9225d0fb4ac18bb423ee94f23ffaa3fd16d77f22
DESKTOP-4QCTMKG\SSAFY <ucs210212@naver.com>
2025-04-29 17:13:21 +0900

    Remove: 컬렉션 도메인 예외 처리 코드 제거

--- back/memozy-api/src/main/java/site/memozy/memozy_api/domain/collection/service/CollectionService.java
+++ back/memozy-api/src/main/java/site/memozy/memozy_api/domain/collection/service/CollectionService.java
@@ -1,20 +1,12 @@
 package site.memozy.memozy_api.domain.collection.service;
 
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
-import site.memozy.memozy_api.domain.collection.exception.CollectionError;
-import site.memozy.memozy_api.domain.collection.exception.CollectionException;
 import site.memozy.memozy_api.domain.collection.repository.CollectionRepository;
 
 @Service
 @RequiredArgsConstructor
 public class CollectionService {
 	private final CollectionRepository collectionRepository;
-
-	@Transactional
-	public void test() {
-		throw new CollectionException(CollectionError.USER_NOT_FOUND);
-	}
 }
