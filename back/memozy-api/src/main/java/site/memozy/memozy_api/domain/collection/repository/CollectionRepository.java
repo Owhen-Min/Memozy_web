e61db07482becb303e1c6221532648f278596307
DESKTOP-4QCTMKG\SSAFY <ucs210212@naver.com>
2025-04-30 18:22:19 +0900

    Refact: Interface위치 변경

--- back/memozy-api/src/main/java/site/memozy/memozy_api/domain/collection/repository/CollectionRepository.java
+++ back/memozy-api/src/main/java/site/memozy/memozy_api/domain/collection/repository/CollectionRepository.java
@@ -1,6 +1,7 @@
 package site.memozy.memozy_api.domain.collection.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -11,4 +12,5 @@ public interface CollectionRepository extends JpaRepository<Collection, Integer>
 
 	List<Collection> findAllByUserId(Integer userId);
 
+	Optional<Collection> findByCollectionIdAndUserId(Integer collectionId, Integer userId);
 }
--- back/memozy-api/src/main/java/site/memozy/memozy_api/domain/collection/repository/CollectionRepositoryCustom.java
+++ back/memozy-api/src/main/java/site/memozy/memozy_api/domain/collection/repository/CollectionRepositoryCustom.java
@@ -1,13 +1,9 @@
 package site.memozy.memozy_api.domain.collection.repository;
 
 import java.util.List;
-import java.util.Optional;
 
 import site.memozy.memozy_api.domain.collection.dto.CollectionSummaryResponse;
-import site.memozy.memozy_api.domain.collection.entity.Collection;
 
 public interface CollectionRepositoryCustom {
 	List<CollectionSummaryResponse> findCollectionSummariesByUserId(Integer userId);
-
-	Optional<Collection> findByCollectionIdAndUserId(Integer collectionId, Integer userId);
 }
