18456123a8eb963e637f49298b083edc1e54bfc5
DESKTOP-4QCTMKG\SSAFY <ucs210212@naver.com>
2025-04-30 16:12:20 +0900

    Feat: Collection Service 추가

--- back/memozy-api/src/main/java/site/memozy/memozy_api/domain/collection/service/CollectionService.java
+++ back/memozy-api/src/main/java/site/memozy/memozy_api/domain/collection/service/CollectionService.java
@@ -1,12 +1,17 @@
 package site.memozy.memozy_api.domain.collection.service;
 
-import org.springframework.stereotype.Service;
+import site.memozy.memozy_api.domain.collection.dto.CollectionCreateRequest;
+import site.memozy.memozy_api.domain.collection.dto.CollectionDeleteRequest;
+import site.memozy.memozy_api.domain.collection.dto.CollectionListResponse;
+import site.memozy.memozy_api.domain.collection.dto.CollectionUpdateRequest;
+import site.memozy.memozy_api.global.auth.CustomOAuth2User;
 
-import lombok.RequiredArgsConstructor;
-import site.memozy.memozy_api.domain.collection.repository.CollectionRepository;
+public interface CollectionService {
+	void createCollection(CustomOAuth2User user, CollectionCreateRequest request);
 
-@Service
-@RequiredArgsConstructor
-public class CollectionService {
-	private final CollectionRepository collectionRepository;
+	void deleteCollection(CustomOAuth2User user, CollectionDeleteRequest request);
+
+	void updateCollection(CustomOAuth2User user, Integer CollectionId, CollectionUpdateRequest request);
+
+	CollectionListResponse getAllCollections(CustomOAuth2User user);
 }
