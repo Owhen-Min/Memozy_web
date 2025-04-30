dbe7f24a93e06853b0e7fc2c2c05a27998d097b7
DESKTOP-4QCTMKG\SSAFY <ucs210212@naver.com>
2025-04-30 16:13:59 +0900

    Chore: Quiz 클래스 위치 변경

--- back/memozy-api/src/main/java/site/memozy/memozy_api/domain/quizsource/entity/QuizSource.java
+++ back/memozy-api/src/main/java/site/memozy/memozy_api/domain/quizsource/entity/QuizSource.java
@@ -1,9 +1,7 @@
-package site.memozy.memozy_api.domain.quiz;
+package site.memozy.memozy_api.domain.quizsource.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -17,37 +15,31 @@ import site.memozy.memozy_api.global.audit.BaseTimeEntity;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Table(name = "quizzes",
+@Table(name = "quiz_sources",
 	indexes = {
-		@Index(name = "idx_collection_id", columnList = "collection_id"),
-		@Index(name = "idx_source_id", columnList = "source_id")
+		@Index(name = "idx_user_id", columnList = "user_id"),
+		@Index(name = "idx_collection_id", columnList = "collection_id")
 	}
 )
-public class Quiz extends BaseTimeEntity {
+public class QuizSource extends BaseTimeEntity {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long quizId;
-
-	@Column(nullable = false, columnDefinition = "TEXT")
-	private String content;
-
-	@Enumerated(EnumType.STRING)
-	private QuizType type;
+	private Integer sourceId;
 
 	@Column(nullable = false)
-	private String answer;
+	private String title;
 
 	@Column(nullable = false, columnDefinition = "TEXT")
-	private String commentary;
+	private String summary;
 
-	@Column(name = "quiz_option", columnDefinition = "TEXT")
-	private String option;
+	@Column(nullable = false, length = 1024)
+	private String url;
+
+	@Column(nullable = false)
+	private Integer userId;
 
 	@Column(nullable = false)
 	private Integer collectionId;
-
-	@Column(nullable = false)
-	private Integer sourceId;
 
 }
