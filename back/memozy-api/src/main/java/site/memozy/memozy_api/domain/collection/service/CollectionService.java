01044bfc8e4a03702a13463e3b7636dc4bacae7c
DESKTOP-4QCTMKG\SSAFY <ucs210212@naver.com>
2025-04-30 17:28:01 +0900

    Refactor: 컬렉션 조회 데이터 출력 변경

--- back/memozy-api/src/main/java/site/memozy/memozy_api/domain/collection/service/CollectionService.java
+++ back/memozy-api/src/main/java/site/memozy/memozy_api/domain/collection/service/CollectionService.java
@@ -1,8 +1,10 @@
 package site.memozy.memozy_api.domain.collection.service;
 
+import java.util.List;
+
 import site.memozy.memozy_api.domain.collection.dto.CollectionCreateRequest;
 import site.memozy.memozy_api.domain.collection.dto.CollectionDeleteRequest;
-import site.memozy.memozy_api.domain.collection.dto.CollectionListResponse;
+import site.memozy.memozy_api.domain.collection.dto.CollectionSummaryResponse;
 import site.memozy.memozy_api.domain.collection.dto.CollectionUpdateRequest;
 import site.memozy.memozy_api.global.auth.CustomOAuth2User;
 
@@ -13,5 +15,5 @@ public interface CollectionService {
 
 	void updateCollection(CustomOAuth2User user, Integer CollectionId, CollectionUpdateRequest request);
 
-	CollectionListResponse getAllCollections(CustomOAuth2User user);
+	List<CollectionSummaryResponse> getAllCollections(CustomOAuth2User user);
 }
