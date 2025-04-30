1ddb4ee3321bd290e1c175cf4fbdd42b9ac1d6f9
DESKTOP-4QCTMKG\SSAFY <ucs210212@naver.com>
2025-04-30 16:12:02 +0900

    Feat: Collection Repository 추가

--- back/memozy-api/src/main/java/site/memozy/memozy_api/domain/collection/repository/CollectionRepository.java
+++ back/memozy-api/src/main/java/site/memozy/memozy_api/domain/collection/repository/CollectionRepository.java
@@ -1,8 +1,14 @@
 package site.memozy.memozy_api.domain.collection.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import site.memozy.memozy_api.domain.collection.entity.Collection;
 
-public interface CollectionRepository extends JpaRepository<Collection, Integer> {
+public interface CollectionRepository extends JpaRepository<Collection, Integer>, CollectionRepositoryCustom {
+	boolean existsByUserIdAndName(Integer userId, String name);
+
+	List<Collection> findAllByUserId(Integer userId);
+
 }
---
+++ back/memozy-api/src/main/java/site/memozy/memozy_api/domain/collection/repository/CollectionRepositoryImpl.java
@@ -0,0 +1,42 @@
+package site.memozy.memozy_api.domain.collection.repository;
+
+import java.util.List;
+
+import com.querydsl.jpa.JPAExpressions;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import lombok.RequiredArgsConstructor;
+import site.memozy.memozy_api.domain.collection.dto.CollectionSummaryResponse;
+import site.memozy.memozy_api.domain.collection.dto.QCollectionSummaryResponse;
+import site.memozy.memozy_api.domain.collection.entity.QCollection;
+import site.memozy.memozy_api.domain.quiz.entity.QQuiz;
+import site.memozy.memozy_api.domain.quizsource.entity.QQuizSource;
+
+@RequiredArgsConstructor
+public class CollectionRepositoryImpl implements CollectionRepositoryCustom {
+	private final JPAQueryFactory queryFactory;
+
+	@Override
+	public List<CollectionSummaryResponse> findCollectionSummariesByUserId(Integer userId) {
+		QCollection collection = QCollection.collection;
+		QQuiz quiz = QQuiz.quiz;
+		QQuizSource quizSource = QQuizSource.quizSource;
+
+		return queryFactory
+			.select(new QCollectionSummaryResponse(
+				collection.collectionId,
+				collection.name,
+				// quiz count
+				JPAExpressions.select(quiz.count())
+					.from(quiz)
+					.where(quiz.collectionId.eq(collection.collectionId)),
+				// source(memozy) count
+				JPAExpressions.select(quizSource.count())
+					.from(quizSource)
+					.where(quizSource.collectionId.eq(collection.collectionId))
+			))
+			.from(collection)
+			.where(collection.userId.eq(userId))
+			.fetch();
+	}
+}
