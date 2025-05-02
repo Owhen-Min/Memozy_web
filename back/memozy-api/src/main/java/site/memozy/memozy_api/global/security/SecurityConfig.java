a8c87f47baf2aaf9f82b8e06524800c5d71ff139
TinyFrogs <qns0147@gmail.com>
2025-05-02 17:22:54 +0900

    [CHORE] Security 설정 추가

--- back/memozy-api/src/main/java/site/memozy/memozy_api/global/security/SecurityConfig.java
+++ back/memozy-api/src/main/java/site/memozy/memozy_api/global/security/SecurityConfig.java
@@ -89,6 +89,7 @@ public class SecurityConfig {
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
 		configuration.setAllowedOrigins(Arrays.asList(
+			"chrome-extension://dfghbgncpceajjhnkmfinhmdafmkglak",
 			"http://localhost:5173",
 			"https://localhost:5173",
 			"http://memozy.site",
