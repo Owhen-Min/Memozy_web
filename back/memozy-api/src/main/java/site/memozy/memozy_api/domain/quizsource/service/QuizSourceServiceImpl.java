3633f4fe0597f6a0a1b05dc7288e5315fc195beb
TinyFrogs <qns0147@gmail.com>
2025-05-06 22:16:12 +0900

    [FEAT] Quiz 생성 및 조회 서비스 컨트롤러 구현

--- back/memozy-api/src/main/java/site/memozy/memozy_api/domain/quizsource/service/QuizSourceServiceImpl.java
+++ back/memozy-api/src/main/java/site/memozy/memozy_api/domain/quizsource/service/QuizSourceServiceImpl.java
@@ -3,11 +3,8 @@ package site.memozy.memozy_api.domain.quizsource.service;
 import static site.memozy.memozy_api.global.payload.code.ErrorStatus.QUIZ_SOURCE_EXISTS;
 import static site.memozy.memozy_api.global.payload.code.ErrorStatus.QUIZ_SOURCE_NOT_FOUND;
 
-import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,6 +12,7 @@ import site.memozy.memozy_api.domain.quizsource.dto.QuizSourceCreateRequest;
 import site.memozy.memozy_api.domain.quizsource.dto.QuizSourceResponse;
 import site.memozy.memozy_api.domain.quizsource.entity.QuizSource;
 import site.memozy.memozy_api.domain.quizsource.repository.QuizSourceRepository;
+import site.memozy.memozy_api.global.openai.OpenAiService;
 import site.memozy.memozy_api.global.payload.exception.GeneralException;
 
 @Slf4j
@@ -22,33 +20,12 @@ import site.memozy.memozy_api.global.payload.exception.GeneralException;
 @RequiredArgsConstructor
 public class QuizSourceServiceImpl implements QuizSourceService {
 
-	private final ChatClient chatClient;
-	private final ObjectMapper objectMapper;
+	private final OpenAiService openAiService;
 	private final QuizSourceRepository quizSourceRepository;
 
 	@Override
 	public String summarizeMarkdown(QuizSourceCreateRequest request) {
-		String promptText = """
-			당신의 임무는 받는 데이터를 정리해주는 것입니다.
-			# Steps
-			1. **주어진 데이터를 분석하여 해당 데이터에서 어떤 주제인지 확인**
-			    - 입력한 데이터를 분석하여 어떤 주제의 내용인지 확인합니다.
-			    - 사진의 URL을 통하여 분석하여 해당 이미지가 적절한 이미지 인지 판단합니다.
-			
-			2. **분석한 주제를 통하여 마크다운 형식으로 응답 반환**
-			    - 적절한 이미지가 아니라면 해당 URL은 넣지 않습니다.
-			    - 적절한 이미지라면 해당 URL은 적절한 위치에 넣습니다.
-			    - 데이터가 하나의 주제로 정리하여 생성합니다.
-			
-			# Notes
-			    - 데이터가 하나의 주제로 파악하기 위해 논리적이고 체계적인 접근 방식을 사용하세요.
-			""" + request.getContext();
-
-		return chatClient
-			.prompt()
-			.user(promptText)
-			.call()
-			.content();
+		return openAiService.summarizeMarkdown(request);
 	}
 
 	@Override
