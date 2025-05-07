ebf3f279cbb817fea258a40c615d4fe4e9e007a4
DESKTOP-4QCTMKG\SSAFY <ucs210212@naver.com>
2025-05-07 14:21:51 +0900

    feat: 개인 퀴즈쇼(Repository)

--- back/memozy-api/src/main/java/site/memozy/memozy_api/domain/quiz/repository/QuizRepositoryCustomImpl.java
+++ back/memozy-api/src/main/java/site/memozy/memozy_api/domain/quiz/repository/QuizRepositoryCustomImpl.java
@@ -1,10 +1,18 @@
 package site.memozy.memozy_api.domain.quiz.repository;
 
+import static site.memozy.memozy_api.domain.history.QHistory.*;
+import static site.memozy.memozy_api.domain.quiz.entity.QQuiz.*;
+import static site.memozy.memozy_api.domain.quizsource.entity.QQuizSource.*;
+
 import java.util.List;
 
+import com.querydsl.core.types.dsl.Expressions;
+import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import lombok.RequiredArgsConstructor;
+import site.memozy.memozy_api.domain.quiz.dto.PersonalQuizResponse;
+import site.memozy.memozy_api.domain.quiz.dto.QPersonalQuizResponse;
 import site.memozy.memozy_api.domain.quiz.dto.QQuizSelectResponse;
 import site.memozy.memozy_api.domain.quiz.dto.QuizSelectResponse;
 import site.memozy.memozy_api.domain.quiz.entity.QQuiz;
@@ -26,6 +34,46 @@ public class QuizRepositoryCustomImpl implements QuizRepositoryCustom {
 			))
 			.from(quiz)
 			.where(quiz.sourceId.eq(sourceId))
+			.fetch();
+	}
+
+	@Override
+	public List<PersonalQuizResponse> getPersonalQuizzes(int userId, int collectionId, int count, boolean newOnly) {
+		// 1. QuizSource 유효성 검사
+		List<Integer> sourceIds = jpaQueryFactory
+			.select(quizSource.sourceId)
+			.from(quizSource)
+			.where(
+				quizSource.collectionId.eq(collectionId),
+				quizSource.userId.eq(userId)
+			)
+			.fetch();
+
+		// 2. 랜덤 퀴즈 조회
+		return jpaQueryFactory
+			.select(new QPersonalQuizResponse(
+				quiz.quizId,
+				quiz.content,
+				quiz.type,
+				quiz.answer,
+				quiz.commentary,
+				quiz.option
+			))
+			.from(quiz)
+			.where(
+				quiz.sourceId.in(sourceIds),
+				newOnly ? quiz.quizId.notIn(
+					JPAExpressions
+						.select(history.quizId)
+						.from(history)
+						.where(
+							history.collectionId.eq(collectionId),
+							history.quizId.isNotNull()
+						)
+				) : null // newOnly가 false일 때는 조건 생략
+			)
+			.orderBy(Expressions.numberTemplate(Double.class, "RAND()").asc()) // 랜덤 정렬
+			.limit(count)
 			.fetch();
 	}
 }
