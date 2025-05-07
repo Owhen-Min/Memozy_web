4de6edb136d928a317a5bb1551fcf060abc9120a
박태현 <rinma772@naver.com>
2025-05-07 15:49:23 +0900

    [FIX] run build 오류 수정

--- front/web/src/features/targetCollectionPage/CopyMemozyModal.tsx
+++ front/web/src/features/targetCollectionPage/CopyMemozyModal.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { collectionData } from '../../dummy/collectionData';
 
 interface CopyMemozyModalProps {
--- front/web/src/features/targetCollectionPage/DeleteMemozyModal.tsx
+++ front/web/src/features/targetCollectionPage/DeleteMemozyModal.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+
 
 interface DeleteMemozyModalProps {
     memozyIds: number[];
