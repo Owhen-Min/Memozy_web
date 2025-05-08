b17ad637befd6a996989565d580f544107223460
TinyFrogs <qns0147@gmail.com>
2025-05-08 10:40:24 +0900

    [FEAT] 퀴즈 개수, 풀었던 퀴즈 개수 조회 레포지토리 구현

--- back/memozy-api/src/main/java/site/memozy/memozy_api/domain/history/repository/HistoryRepository.java
+++ back/memozy-api/src/main/java/site/memozy/memozy_api/domain/history/repository/HistoryRepository.java
@@ -1,8 +1,16 @@
 package site.memozy.memozy_api.domain.history.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import site.memozy.memozy_api.domain.history.entity.History;
 
 public interface HistoryRepository extends JpaRepository<History, Integer>, HistoryCustomRepository {
+
+	@Query("SELECT COUNT(DISTINCT h.quizId) FROM History h WHERE h.collectionId IN :collectionIds")
+	long countDistinctSolvedQuiz(@Param("collectionIds") List<Integer> collectionIds);
+
 }
