3281cd7d41d29b83b7ae5a7d5bee42adf5d53f07
TinyFrogs <qns0147@gmail.com>
2025-05-08 13:39:37 +0900

    [FEAT] QueryDSL로 존재하는 오답 컬렉션 조회 기능 구현

--- back/memozy-api/src/main/java/site/memozy/memozy_api/domain/collection/repository/CollectionRepositoryCustom.java
+++ back/memozy-api/src/main/java/site/memozy/memozy_api/domain/collection/repository/CollectionRepositoryCustom.java
@@ -3,7 +3,10 @@ package site.memozy.memozy_api.domain.collection.repository;
 import java.util.List;
 
 import site.memozy.memozy_api.domain.collection.dto.CollectionSummaryResponse;
+import site.memozy.memozy_api.domain.collection.dto.UnsolvedCollectionDtoResponse;
 
 public interface CollectionRepositoryCustom {
 	List<CollectionSummaryResponse> findCollectionSummariesByUserId(Integer userId);
+
+	List<UnsolvedCollectionDtoResponse> findUnsolvedCollectionsByUserId(Integer userId);
 }
--- back/memozy-api/src/main/java/site/memozy/memozy_api/domain/collection/repository/CollectionRepositoryImpl.java
+++ back/memozy-api/src/main/java/site/memozy/memozy_api/domain/collection/repository/CollectionRepositoryImpl.java
@@ -2,13 +2,16 @@ package site.memozy.memozy_api.domain.collection.repository;
 
 import java.util.List;
 
+import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import lombok.RequiredArgsConstructor;
 import site.memozy.memozy_api.domain.collection.dto.CollectionSummaryResponse;
 import site.memozy.memozy_api.domain.collection.dto.QCollectionSummaryResponse;
+import site.memozy.memozy_api.domain.collection.dto.UnsolvedCollectionDtoResponse;
 import site.memozy.memozy_api.domain.collection.entity.QCollection;
+import site.memozy.memozy_api.domain.history.entity.QHistory;
 import site.memozy.memozy_api.domain.quiz.entity.QQuiz;
 import site.memozy.memozy_api.domain.quizsource.entity.QQuizSource;
 
@@ -37,6 +40,30 @@ public class CollectionRepositoryImpl implements CollectionRepositoryCustom {
 			))
 			.from(collection)
 			.where(collection.userId.eq(userId))
+			.fetch();
+	}
+
+	@Override
+	public List<UnsolvedCollectionDtoResponse> findUnsolvedCollectionsByUserId(Integer userId) {
+		QCollection collection = QCollection.collection;
+		QHistory history = QHistory.history;
+
+		return queryFactory
+			.select(Projections.constructor(UnsolvedCollectionDtoResponse.class,
+				collection.collectionId,
+				collection.name
+			))
+			.from(collection)
+			.where(
+				collection.userId.eq(userId),
+				JPAExpressions.selectOne()
+					.from(history)
+					.where(
+						history.collectionId.eq(collection.collectionId),
+						history.isSolved.isFalse()
+					)
+					.exists()
+			)
 			.fetch();
 	}
 }
