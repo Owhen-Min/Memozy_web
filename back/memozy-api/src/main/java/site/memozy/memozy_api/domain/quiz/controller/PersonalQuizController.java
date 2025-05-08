df1ddc5a16d5e6831b1b81b847622da2a2e25237
DESKTOP-4QCTMKG\SSAFY <ucs210212@naver.com>
2025-05-08 12:39:57 +0900

    Feat: 개인 퀴즈쇼(Contorller)

--- back/memozy-api/src/main/java/site/memozy/memozy_api/domain/quiz/controller/PersonalQuizController.java
+++ back/memozy-api/src/main/java/site/memozy/memozy_api/domain/quiz/controller/PersonalQuizController.java
@@ -1,10 +1,10 @@
 package site.memozy.memozy_api.domain.quiz.controller;
 
-import java.util.List;
-
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +13,10 @@ import groovy.util.logging.Slf4j;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import site.memozy.memozy_api.domain.quiz.dto.PersonalQuizResponse;
+import site.memozy.memozy_api.domain.quiz.dto.PersonalQuizAndSessionResponse;
+import site.memozy.memozy_api.domain.quiz.dto.PersonalQuizAnswerRequest;
+import site.memozy.memozy_api.domain.quiz.dto.PersonalQuizResultRequest;
+import site.memozy.memozy_api.domain.quiz.dto.PersonalQuizResultResponse;
 import site.memozy.memozy_api.domain.quiz.service.PersonalQuizService;
 import site.memozy.memozy_api.global.payload.ApiResponse;
 import site.memozy.memozy_api.global.security.auth.CustomOAuth2User;
@@ -27,14 +30,36 @@ public class PersonalQuizController {
 	private final PersonalQuizService personalQuizService;
 
 	@GetMapping("/{collectionId}")
-	public ApiResponse<List<PersonalQuizResponse>> getPersonalQuizzes(
+	public ApiResponse<PersonalQuizAndSessionResponse> getPersonalQuizzes(
 		@Parameter(hidden = true) @AuthenticationPrincipal CustomOAuth2User user,
 		@PathVariable Integer collectionId,
 		@RequestParam(defaultValue = "10") int count,
 		@RequestParam(defaultValue = "false") boolean newOnly) {
 
-		List<PersonalQuizResponse> responses = personalQuizService.getPersonalQuizzes(user.getUserId(), collectionId,
+		PersonalQuizAndSessionResponse responses = personalQuizService.getPersonalQuizzes(user.getUserId(),
+			collectionId,
 			count, newOnly);
 		return ApiResponse.success(responses);
 	}
+
+	@PostMapping("/{quiz_id}")
+	public ApiResponse<Void> submitPersonalQuiz(
+		@Parameter(hidden = true) @AuthenticationPrincipal CustomOAuth2User user,
+		@PathVariable("quiz_id") Long quizId,
+		@RequestBody PersonalQuizAnswerRequest request) {
+		personalQuizService.submitQuizAnswer(user.getUserId(), quizId, request);
+		return ApiResponse.success();
+	}
+
+	@PostMapping("/result/{collectionId}")
+	public ApiResponse<PersonalQuizResultResponse> getPersonalQuizResult(
+		@Parameter(hidden = true) @AuthenticationPrincipal CustomOAuth2User user,
+		@PathVariable Integer collectionId,
+		@RequestBody PersonalQuizResultRequest request) {
+
+		PersonalQuizResultResponse response = personalQuizService.getPersonalQuizResult(user.getUserId(),
+			collectionId, request.getQuizSessionId());
+		return ApiResponse.success(response);
+	}
+
 }
