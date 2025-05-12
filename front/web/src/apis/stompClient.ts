cbddc7ae0053093c707f049556908776a6029c12
Owhen-Min <skyblue847@hanyang.ac.kr>
2025-05-12 14:27:25 +0900

    feat: stompClient 기본 설정

---
+++ front/web/src/apis/stompClient.ts
@@ -0,0 +1,51 @@
+import { useEffect, useState, useRef } from "react";
+import { Client } from "@stomp/stompjs";
+
+const useWebSocket = () => {
+  const [isConnected, setIsConnected] = useState(false);
+  const [stompClient, setStompClient] = useState<Client | null>(null);
+  const clientRef = useRef<Client | null>(null);
+
+  useEffect(() => {
+    const accessToken = localStorage.getItem("memozy_access_token");
+    const client = new Client({
+      brokerURL: "wss://memozy.site/ws",
+      reconnectDelay: 5000,
+      heartbeatIncoming: 10000,
+      heartbeatOutgoing: 10000,
+      connectHeaders: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+
+      onConnect: () => {
+        console.log("웹소켓 연결");
+        setIsConnected(true);
+        setStompClient(client);
+      },
+      onDisconnect: () => {
+        console.log("웹소켓 연결 종료");
+        setIsConnected(false);
+        setStompClient(null);
+      },
+      onStompError: (frame) => {
+        console.error("에러 발생:", frame);
+      },
+    });
+
+    client.activate();
+    clientRef.current = client;
+
+
+    return () => {
+      if (clientRef.current) {
+        clientRef.current.deactivate();
+        setIsConnected(false);
+        setStompClient(null);
+      }
+    };
+  }, []);
+
+  return { stompClient, isConnected };
+};
+
+export default useWebSocket;
