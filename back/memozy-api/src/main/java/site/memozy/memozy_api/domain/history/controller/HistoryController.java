f82b4e01b45401ee383423af8afe14caf22c0853
TinyFrogs <qns0147@gmail.com>
2025-05-14 12:51:52 +0900

    [Feat] 히스토리 학습 참여도 조회 반환 값 변경

--- back/memozy-api/src/main/java/site/memozy/memozy_api/domain/history/controller/HistoryController.java
+++ back/memozy-api/src/main/java/site/memozy/memozy_api/domain/history/controller/HistoryController.java
@@ -6,7 +6,6 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -15,7 +14,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import site.memozy.memozy_api.domain.history.dto.HistoryCollectionStatsResponse;
-import site.memozy.memozy_api.domain.history.dto.HistoryContributeResponse;
+import site.memozy.memozy_api.domain.history.dto.LearningContributionResponse;
 import site.memozy.memozy_api.domain.history.dto.QuizStatsResponse;
 import site.memozy.memozy_api.domain.history.dto.UnsolvedCollectionDtoResponse;
 import site.memozy.memozy_api.domain.history.entity.CollectionHistoryDetailResponse;
@@ -34,11 +33,10 @@ public class HistoryController {
 
 	@Operation(summary = "학습 참여도 조회", description = "년도별 학습 참여도를 조회할 수 있습니다.")
 	@GetMapping("/streaks")
-	public ApiResponse<List<HistoryContributeResponse>> getHistory(
-		@Parameter(hidden = true) @AuthenticationPrincipal CustomOAuth2User customOAuth2User,
-		@Parameter(description = "조회할 년도, 입력하지 않으면 최근 12개월") @RequestParam(value = "year", required = false) Integer year
-	) {
-		List<HistoryContributeResponse> history = historyService.getUserStreaks(customOAuth2User.getUserId(), year);
+	public ApiResponse<LearningContributionResponse> getHistory(
+		@Parameter(hidden = true) @AuthenticationPrincipal CustomOAuth2User customOAuth2User) {
+		LearningContributionResponse history = historyService.getUserStreaks(customOAuth2User.getUserId(),
+			customOAuth2User.getEmail());
 		return ApiResponse.success(history);
 	}
 
