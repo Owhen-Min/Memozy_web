f81d184b689fc1ebfb8ed0b646da079069b284af
박태현 <rinma772@naver.com>
2025-05-16 15:44:29 +0900

    [FIX] handleOutsideClick 로직 수정(컬렉션페이지에있는 모달에만 연결)

--- front/web/src/pages/CollectionPage.tsx
+++ front/web/src/pages/CollectionPage.tsx
@@ -30,8 +30,8 @@ function CollectionPage() {
 
   // 바깥 영역 클릭 시 모달 닫기 처리
   const handleOutsideClick = (e: React.MouseEvent<HTMLDivElement>) => {
-    // 현재 이벤트가 발생한 요소가 바깥 영역(overlay)인 경우에만 닫기
-    if (e.currentTarget === e.target) {
+    // mousedown 이벤트이고, 현재 이벤트가 발생한 요소가 바깥 영역(overlay)인 경우에만 닫기
+    if (e.type === "mousedown" && e.currentTarget === e.target) {
       handleCloseModal();
     }
   };
@@ -104,7 +104,7 @@ function CollectionPage() {
       {isAddCollectionModalOpen && (
         <div
           className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50"
-          onClick={handleOutsideClick}
+          onMouseDown={handleOutsideClick}
         >
           <AddCollection isOpen={isAddCollectionModalOpen} onClose={handleCloseModal} />
         </div>
