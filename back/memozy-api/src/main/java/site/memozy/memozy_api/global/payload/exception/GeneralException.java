7442867bb94e669f75f99eecf9fc3811051f8b62
TinyFrogs <qns0147@gmail.com>
2025-05-16 16:11:02 +0900

    [FIX] AllArgsConstructor 대신 생성자 구현

--- back/memozy-api/src/main/java/site/memozy/memozy_api/global/payload/exception/GeneralException.java
+++ back/memozy-api/src/main/java/site/memozy/memozy_api/global/payload/exception/GeneralException.java
@@ -1,13 +1,20 @@
 package site.memozy.memozy_api.global.payload.exception;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import site.memozy.memozy_api.global.payload.code.BaseErrorCode;
 
 @Getter
-@AllArgsConstructor
 public class GeneralException extends RuntimeException {
 
 	private final transient BaseErrorCode code;
+
+	public GeneralException(BaseErrorCode code) {
+		super(code.getErrorMsg());
+		this.code = code;
+	}
+
+	public BaseErrorCode getCode() {
+		return code;
+	}
 
 }
