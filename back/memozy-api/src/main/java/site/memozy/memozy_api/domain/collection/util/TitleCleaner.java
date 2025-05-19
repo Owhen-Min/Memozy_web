4b762936e3cc215cb9ee7b22f6e06e854d23d759
DESKTOP-4QCTMKG\SSAFY <ucs210212@naver.com>
2025-05-19 10:33:46 +0900

    [FIX] 정규표현식 변경

--- back/memozy-api/src/main/java/site/memozy/memozy_api/domain/collection/util/TitleCleaner.java
+++ back/memozy-api/src/main/java/site/memozy/memozy_api/domain/collection/util/TitleCleaner.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class TitleCleaner {
-	private static final Pattern COPY_TITLE_PATTERN = Pattern.compile("\\(복사본\\s*-\\s*(.*?)\\)$");
+	private static final Pattern COPY_TITLE_PATTERN = Pattern.compile("\\(복사본\\s*-\\s*([^()]+)\\)$");
 
 	public String removeCopySuffix(String title) {
 		return COPY_TITLE_PATTERN.matcher(title).replaceAll("").trim();
