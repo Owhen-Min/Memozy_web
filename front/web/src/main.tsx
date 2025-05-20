ccc962aa75ad9d0ed5b68810aa28cb7a27eb1b5e
Owhen-Min <skyblue847@hanyang.ac.kr>
2025-05-20 13:46:22 +0900

    fix: deleted unused value

--- front/web/src/main.tsx
+++ front/web/src/main.tsx
@@ -5,7 +5,7 @@ import App from "./App.tsx";
 import "./styles/global.css";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  // <StrictMode>
+  <App />
+  // </StrictMode>
 );
